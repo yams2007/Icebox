c8623b87956b2a9ccf6957ff0b0f36b2b1dcc693
amagiri <ssakuda@outlook.com>
2015-02-01 18:30:35 -0800

    Adjusted navbar contents

--- Icebox/Controllers/HomeController.cs
+++ Icebox/Controllers/HomeController.cs
@@ -8,21 +8,35 @@ namespace Icebox.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Recent()
+        {
+            return View();
+        }        
+
+        public ActionResult Calendar()
         {
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            ViewBag.Message = "Your search page.";
 
             return View();
         }
