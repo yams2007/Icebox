03750e3a85c14d174217581741e8de9cb77b9a12
Michael Yamamoto <miyamamo@microsoft.com>
2015-02-15 23:16:56 -0800

    Intermediate changes to begin adding differentiated content types. Need to investigate best practices for transforming data between types.

---
+++ Icebox/Models/PostViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Icebox.Models
+{
+    public class PostViewModel
+    {
+    }
+
+    public enum ContentType
+    {
+        Image,
+        Text,
+        Video
+    }
+
+    public class Content
+    {
+        public ICollection<String> Tags { get; set; }
+        public ContentType ContentType { get; set; }
+    }
+
+    public class ImageContent : Content
+    {
+        this.ContentType = ContentType.Image;
+
+        public String Url { get; set; }
+    }
+
+    public class IContentItem
+    {
+        public ContentType ItemType { get; set; }
+        public Content Data { get; set; }
+
+    }
+}
